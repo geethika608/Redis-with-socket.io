1aec020dc45520cd8f84ab6f4781854a08929751
geethika <geethikad@geethikas-MacBook-Pro.local>
2016-11-25 13:59:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Increasing Socket.io performance with the use of Redis
+
+This Project uses a redis adapter to bind with socket.io to run multiple socket.io instances in different processes or servers that can all broadcast and emit events to and from each other
---
+++ socketio-redis.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
+var redis = require('socket.io-redis');
+//install redis and when run it,It runs on a default port 6379
+io.adapter(redis({host: 'localhost', port: 6379}));
