7e49e07cc0a6c67c05bacd1767a215c278696324
geethika <geethu608@gmail.com>
2016-11-29 12:37:54 +0530

    Update socketio-redis.js

--- socketio-redis.js
+++ socketio-redis.js
@@ -5,3 +5,7 @@ var io = require('socket.io').listen(http);
 var redis = require('socket.io-redis');
 //install redis and when run it,It runs on a default port 6379
 io.adapter(redis({host: 'localhost', port: 6379}));
+
+http.listen(3000, 'Your Ip Address Goes Here', function () {
+    console.log('listening on *:3000');
+});
